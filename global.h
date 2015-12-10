68e77017c341650a0ccd1b23d4653eed6e38f3a1
Sean <sturner@nmt.edu>
2015-12-10 00:22:30 -0700

    add helper things to header

--- global.h
+++ global.h
@@ -2,7 +2,24 @@
 // Created by Sean Turner on 12/10/15.
 //
 
-#ifndef LLVM_KALEIDESCOPE_LEXER_H
-#define LLVM_KALEIDESCOPE_LEXER_H
+#pragma once
 
-#endif //LLVM_KALEIDESCOPE_LEXER_H
+#include <cctype>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+
+// LLVM uses their own make_unique in the tutorial. We'll reproduce it here,
+// but may not use it! The helper namespace might be useful anyway
+namespace helper {
+	template <class T, class... Args>
+	static
+		typename std::enable_if<!std::is_array<T>::value, std::unique_ptr<T>>::type
+		make_unique(Args &&... args) {
+	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+};
+}
+
+
