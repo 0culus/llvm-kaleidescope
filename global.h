396c1fe62bff97da53a17ee065174b246f00e9a2
Sean <sturner@nmt.edu>
2015-12-10 01:08:25 -0700

    to spaces

--- global.h
+++ global.h
@@ -14,11 +14,11 @@
 // LLVM uses their own make_unique in the tutorial. We'll reproduce it here,
 // but may not use it! The helper namespace might be useful anyway
 namespace helper {
-	template <class T, class... Args>
-	static
-		typename std::enable_if<!std::is_array<T>::value, std::unique_ptr<T>>::type
-		make_unique(Args &&... args) {
-	return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+  template <class T, class... Args>
+  static
+    typename std::enable_if<!std::is_array<T>::value, std::unique_ptr<T>>::type
+    make_unique(Args &&... args) {
+  return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
 };
 }
 
@@ -26,13 +26,13 @@ namespace helper {
  * Here we define all the tokens we need; add to this to add more!
  */
 enum class Token {
-	tok_eof = -1,
+  tok_eof = -1,
 
-	tok_def = -2,
-	tok_extern = -3,
+  tok_def = -2,
+  tok_extern = -3,
 
-	tok_identifier = -4,
-	tok_double = -5
+  tok_identifier = -4,
+  tok_double = -5
 };
 
 static std::string IdentifierStr; // filled if tok_identifier
